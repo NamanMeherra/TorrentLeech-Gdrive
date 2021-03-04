7c89b97e33968a0035032d714cbb9ee38258f738
NamanMeherra <naman.meherra0@gmail.com>
2021-03-05 00:07:31 +0530

    Create config.py

---
+++ tobrot/config.py
@@ -0,0 +1,18 @@
+from tobrot.sample_config import Config
+
+class Config(Config):
+  TG_BOT_TOKEN = "1609250159:AAEGka6ar9rU9mpTZC-l-DK772Jzs6zLPpg"
+  APP_ID = 2093599
+  API_HASH = "da8efde6a2bde78a6542aea1259c752b"
+  AUTH_CHANNEL = [-1001167202534, 1001123370]
+  INDEX_LINK = ""
+  GLEECH_COMMAND = "gleech@leech_bughunterbot"
+  YTDL_COMMAND = 'ytdl@leech_bughunterbot'
+  TELEGRAM_LEECH_COMMAND_G = "tleech@leech_bughunterbot"
+  CLONE_COMMAND_G = "gclone@leech_bughunterbot"
+  PYTDL_COMMAND_G = "pytdl@leech_bughunterbot"
+  DESTINATION_FOLDER = "Gdrive bot"
+  LEECH_COMMAND = "leech@torhunter2_bot"
+  CANCEL_COMMAND_G = "cancel@leech_bughunterbot"
+  RCLONE_CONFIG = """"
+  
